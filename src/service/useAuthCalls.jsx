226ed292c29c154a3b415ec40d692fd5d5d30ce9
mrlmhl <meralorguc1@gmail.com>
2024-01-09 16:59:23 +0300

    merrr

---
+++ src/service/useAuthCalls.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
+import { useNavigate } from "react-router-dom";
+import { fetchFail, fetchStart, loginSuccess } from "../features/authSlice";
+import {useDispatch} from "react-redux"
+
+
+
+const useAuthCalls = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+   const login = async (userInfo) => {
+    dispatch (fetchStart())
+   
+    try {
+    const { data } = await axios.post(
+      `${process.env.REACT_APP_BASE_URL}/auth/login/`,
+      userInfo
+    )
+    dispatch(loginSuccess(data))
+    toastSuccessNotify("Login Başarılı")
+    navigate("/stock")
+    
+  } catch (error) {
+    dispatch(fetchFail())
+    toastErrorNotify("Login işlemi başarısız.");
+    console.log(error);
+  }
+};
+const register = async() => {}
+const logout = async () => {}
+  return {login, register, logout}
+}
+
+export default useAuthCalls;
+
